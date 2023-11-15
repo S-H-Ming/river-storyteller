84f550548515cf145cc6635821396bb860216497
Gurden Batra <gurden.batra@gmail.com>
2023-11-15 16:21:53 +0100

    Prettier formatting improve

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -5,15 +5,18 @@ import ChatComponent from "@/components/chatComponent";
 
 export default function Chat() {
   const searchParams = useSearchParams();
-  const name = searchParams.get("name") || '';
+  const name = searchParams.get("name") || "";
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="bg-[#f3faf9] w-full rounded-md text-grey-900 p-5">
         <h3 className="mb-5">
-          Hello {name}, <br></br> This is Tsen-wen StoryTeller. I am an expert on the story of the river. You can ask me anything! Or if you mention the word &#39;rock&#39;, my friend will appear and share its perspective as well.
+          Hello {name}, <br></br> This is Tsen-wen StoryTeller. I am an expert
+          on the story of the river. You can ask me anything! Or if you mention
+          the word &#39;rock&#39;, my friend will appear and share its
+          perspective as well.
         </h3>
         {/* <div className="mb-5">Hello there, this is Tsen-wen StoryTeller</div> */}
-        <ChatComponent nickname={name}/>
+        <ChatComponent nickname={name} />
       </div>
     </main>
   );
