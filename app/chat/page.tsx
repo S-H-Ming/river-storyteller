29e20df60d8785dc11c7c4d65b5ca60e0a4c3ab5
Gurden Batra <gurden.batra@gmail.com>
2023-11-16 08:37:04 +0100

    make the top header sticky

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -9,13 +9,14 @@ export default function Chat() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="bg-[#f3faf9] w-full rounded-md text-grey-900 p-5">
-        <h3 className="mb-5">
-          Hello {name}, <br></br> This is Tsen-wen StoryTeller. I am an expert
-          on the story of the river. You can ask me anything! Or if you mention
-          the word &#39;rock&#39;, my friend will appear and share its
-          perspective as well.
-        </h3>
-        {/* <div className="mb-5">Hello there, this is Tsen-wen StoryTeller</div> */}
+        <div className="sticky top-0 bg-[#f3faf9] py-2">
+          <h3 className="mb-5">
+            Hello {name}, <br></br> This is Tsen-wen StoryTeller. I am an expert
+            on the story of the river. You can ask me anything! Or if you mention
+            the word &#39;rock&#39;, my friend will appear and share its
+            perspective as well.
+          </h3>
+        </div>
         <ChatComponent nickname={name} />
       </div>
     </main>
