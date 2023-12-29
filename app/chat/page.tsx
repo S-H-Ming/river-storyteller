552348b3c360a5c0cb520a39b0d3446a6031b929
Dennis Sung <red30603@gmail.com>
2023-12-29 11:45:40 +0800

    feat: use riverContract data by url params

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -5,6 +5,7 @@ import ChatComponent from "@/components/chatComponent";
 
 export default function Chat() {
   const searchParams = useSearchParams();
+  const contract = searchParams.get("riverContract") || "";
   const name = searchParams.get("name") || "";
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
@@ -14,7 +15,7 @@ export default function Chat() {
             Hi {name}, <br></br> 我是曾文溪故事的說書人. 我有許多關於曾文溪的故事，問我任何有關曾文溪的任何問題，我都會儘量為您解答！如果您講到任何跟 &#39;石頭&#39; 有關的事情，我的朋友石頭也會現身說法。
           </h3>
         </div>
-        <ChatComponent nickname={name} />
+        <ChatComponent nickname={name} contract={contract}/>
       </div>
     </main>
   );
